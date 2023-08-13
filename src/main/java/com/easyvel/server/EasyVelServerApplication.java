75bf3d9e8a7dc1c9d8b19d1bb1bf1410a00373dd
k0000k <locker50@naver.com>
2023-08-13 15:29:03 +0900

    [CHORE] Initialize repo

---
+++ src/main/java/com/easyvel/server/EasyVelServerApplication.java
@@ -0,0 +1,13 @@
+package com.easyvel.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyVelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyVelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyvel/server/EasyVelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyvel.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyVelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
