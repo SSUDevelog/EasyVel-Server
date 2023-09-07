2b88028d3a9ca8b612f847c3c9e55ee99bd14700
k0000k <locker50@naver.com>
2023-09-07 21:19:15 +0900

    [FIX] apply jwt on subscribe

--- src/main/java/com/easyvel/server/subscribe/SubscribeController.java
+++ src/main/java/com/easyvel/server/subscribe/SubscribeController.java
@@ -1,8 +1,11 @@
 package com.easyvel.server.subscribe;
 
+import com.easyvel.server.annotation.EasyvelTokenApiImplicitParams;
 import com.easyvel.server.global.dto.VelogUserInfoDto;
+import com.easyvel.server.jwt.JwtTokenProvider;
 import com.easyvel.server.subscribe.dto.*;
 import com.easyvel.server.subscribe.service.SubscribeService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,42 +15,24 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("subscribe")
 public class SubscribeController {
 
-//     현재 구현체가 없어서 주석처리 해두었습니다.
-//    private final JwtTokenProvider jwtTokenProvider;
-
+    private final JwtTokenProvider jwtTokenProvider;
     private final SubscribeService subscribeService;
-
 //    알림은 현재 사용하지 않으며, 구현체가 없어서 주석처리합니다.
 //    private final NotificationService notificationService;
-
     private final Logger LOGGER = LoggerFactory.getLogger(SubscribeController.class.getSimpleName());
 
-//    상기 구현체 이유로 주석처리 합니다.
-//    public SubscribeController(JwtTokenProvider jwtTokenProvider, SubscribeService subscribeService, NotificationService notificationService) {
-//        this.jwtTokenProvider = jwtTokenProvider;
-//        this.subscribeService = subscribeService;
-//        this.notificationService = notificationService;
-//    }
-
-    // 리팩토링 진행중에만 임시로 사용하는 생성자입니다.
-    public SubscribeController(SubscribeService subscribeService) {
-        this.subscribeService = subscribeService;
-    }
-
-//    스웨거 설정이 없어서 관련 annotation은 임시로 주석처리 합니다.
-//    @ApiImplicitParams({
-//          @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")})
+    @EasyvelTokenApiImplicitParams
     @GetMapping(value = "/getsubscriber")
     public ResponseEntity<List<VelogUserInfoDto>> getSubscriber(@RequestHeader("X-AUTH-TOKEN") String token) throws IOException {//제네릭 안의 제네릭 -> 별도응답 객체를 만들어야함!
         // 해당 유저가 구독한 대상 리스트를 불러오는 GET api 입니다.
         // throw에 SubscribeException 추가해야 합니다.
 
-        //String userName = jwtTokenProvider.getUsername(token);
-        String userName = "test"; // JWT 구현체가 없어서 임시로 사용합니다.
+        String userName = jwtTokenProvider.getUid(token);
 
         //Todo: 반환 타입을 바꿔서 주기. 구독자 리스트 + 이미지 url 추가해서
 
@@ -56,15 +41,13 @@ public class SubscribeController {
         return ResponseEntity.status(HttpStatus.OK).body(subscribers);
     }
 
-//    스웨거 설정이 없어서 관련 annotation은 임시로 주석처리 합니다.
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")})
+    @EasyvelTokenApiImplicitParams
     @PostMapping(value = "/addsubscriber")
-    public ResponseEntity<String> addSubscriber(@RequestHeader("X-AUTH-TOKEN") String token, @RequestParam String name, @RequestParam String fcmToken) throws IOException{
+    public ResponseEntity<String> addSubscriber(@RequestHeader("X-AUTH-TOKEN") String token, @RequestParam String name, @RequestParam String fcmToken) throws IOException {
         // 유저의 구독자를 추가하는 POST api 입니다.
         // throw에 SubscribeException 추가해야 합니다.
-        // String userName = jwtTokenProvider.getUsername(token);
-        String userName = "test"; // JWT 구현체가 없어서 임시로 사용합니다.
+
+        String userName = jwtTokenProvider.getUid(token);
 
         //JoinGroupDto joinGroupDto = new JoinGroupDto(userName, name); 알림 관련 구현체 없어서 임시 주석 처리
         subscribeService.addSubscribe(userName, name);
@@ -73,25 +56,18 @@ public class SubscribeController {
         return ResponseEntity.status(HttpStatus.OK).body("Success");//임시
     }
 
-//    스웨거 설정이 없어서 관련 annotation은 임시로 주석처리 합니다.
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")})
+    @EasyvelTokenApiImplicitParams
     @GetMapping(value = "/subscriberpost")//responseentity가 없어도 되는지 테스트 되는지 확인해보기//403 402같은 코드를 던질경우 쓰기
-    public ResponseEntity<SubscriberPostsDto> getSubscriberPost(@RequestHeader("X-AUTH-TOKEN") String token) throws IOException{
+    public ResponseEntity<SubscriberPostsDto> getSubscriberPost(@RequestHeader("X-AUTH-TOKEN") String token) throws IOException {
         // 유저가 구독하고 있는 velog 유저들의 post를 리턴하는 GET api입니다.
         // throw에 SubscribeException 추가해야 합니다.
 
-        //String userName = jwtTokenProvider.getUsername(token);
-        String userName = "test"; // JWT 구현체가 없어서 임시로 사용합니다.
-
+        String userName = jwtTokenProvider.getUid(token);
         SubscriberPostsDto subscriberPostsDto = subscribeService.getSubscribersPost(userName);
-
         return ResponseEntity.status(HttpStatus.OK).body(subscriberPostsDto);
     }
 
-//    스웨거 설정이 없어서 관련 annotation은 임시로 주석처리 합니다.
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")})
+    @EasyvelTokenApiImplicitParams
     @GetMapping("/validate/{name}")
     @ResponseBody
     public ResponseEntity<ValidateVelogUserDto> validateUser(@PathVariable String name) throws IOException {
@@ -109,8 +85,7 @@ public class SubscribeController {
     public ResponseEntity<UnsubscribeDto> unSubscribe(@RequestHeader("X-AUTH-TOKEN") String token, @PathVariable String targetName) {
         // 구독을 취소하는 DELETE api입니다.
 
-        //String userName = jwtTokenProvider.getUsername(token);
-        String userName = "test"; // JWT 구현체가 없어서 임시로 사용합니다.
+        String userName = jwtTokenProvider.getUid(token);
         UnsubscribeDto unsubscribeDto = subscribeService.deleteSubscribe(userName, targetName); // 이거 common dto 어떤지?
         subscribeService.validateTarget(targetName);
         return ResponseEntity.status(HttpStatus.OK).body(unsubscribeDto);
@@ -122,20 +97,16 @@ public class SubscribeController {
         // Velog 메인에서 제공하는 실시간 트렌드 포스트들을 리턴합니다.
         // throw에 SubscribeException 추가해야 합니다.
 
-        //String userName = jwtTokenProvider.getUsername(token);
-        String userName = "test"; // JWT 구현체가 없어서 임시로 사용합니다.
+        String userName = jwtTokenProvider.getUid(token);
         try {
             TrendResultDto trendResultDto = subscribeService.collectTrendPost(userName);
             return ResponseEntity.status(HttpStatus.OK).body(trendResultDto);
         } catch (IOException e) {
             throw new IOException(e);
         }
-
     }
 
-//    스웨거 설정이 없어서 관련 annotation은 임시로 주석처리 합니다.
-//    @ApiImplicitParams({
-//            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")})
+    @EasyvelTokenApiImplicitParams
     @GetMapping("/usermain/{name}")
     @ResponseBody
     public UserMainDto userMain(@PathVariable String name) {
