795153e36237aefc3d6e86bb6df0f27d09a21539
kikuke <kkkjs0814@naver.com>
2023-11-14 17:57:21 +0900

    [FIX] url추가

--- src/main/java/com/easyvel/server/velogapi/VelogClient.java
+++ src/main/java/com/easyvel/server/velogapi/VelogClient.java
@@ -19,8 +19,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VelogClient {
 
-    //Todo: pageable 적용하기
     public List<PostDto> getTrendData(int limit, int offset) throws JsonMappingException, JsonProcessingException {
+        String query = "{\"query\":\"\\n    query trendingPosts($input: TrendingPostsInput!) {\\n  trendingPosts(input: $input) {\\n    id\\n    title\\n    short_description\\n    thumbnail\\n    likes\\n    user {\\n      id\\n      username\\n      profile {\\n        id\\n        thumbnail\\n      }\\n    }\\n    url_slug\\n    released_at\\n    updated_at\\n    is_private\\n    comments_count\\n  }\\n}\\n    \",\"variables\":{\"input\":{\"limit\":"+limit+",\"offset\":"+offset+",\"timeframe\":\"week\"}}}";
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://v3.velog.io")
+                .build();
+
+        ResponseEntity<TrendData> response = webClient.post()
+                .uri("/graphql")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(query)
+                .retrieve()
+                .toEntity(TrendData.class)
+                .block();
+
+        List<TrendData.Data.PostData> trendingPosts = response.getBody().getData().getTrendingPosts();
+        List<PostDto> trendPostList = new ArrayList<>();
+
+        for (TrendData.Data.PostData p : trendingPosts) {
+            PostDto postDto = PostDto.builder()
+                    .name(p.getUser().getUsername())
+                    .title(p.getTitle())
+                    .summary(p.getShort_description())
+                    .date(p.getReleased_at())
+                    .comment(p.getComments_count())
+                    .like(p.getLikes())
+                    .img(p.getThumbnail())
+                    .tag(null) // Comment: trend 게시물은 tag가 공백!
+                    .url("velog.io/@"+p.getUser().getUsername()+"/"+p.url_slug)
+                    .build();
+
+            trendPostList.add(postDto);
+        }
+
+        return trendPostList;
+    }
+
+    public List<PostDto> getTagData(int limit, int offset) throws JsonMappingException, JsonProcessingException {
         String query = "{\"query\":\"\\n    query trendingPosts($input: TrendingPostsInput!) {\\n  trendingPosts(input: $input) {\\n    id\\n    title\\n    short_description\\n    thumbnail\\n    likes\\n    user {\\n      id\\n      username\\n      profile {\\n        id\\n        thumbnail\\n      }\\n    }\\n    url_slug\\n    released_at\\n    updated_at\\n    is_private\\n    comments_count\\n  }\\n}\\n    \",\"variables\":{\"input\":{\"limit\":"+limit+",\"offset\":"+offset+",\"timeframe\":\"week\"}}}";
         WebClient webClient = WebClient.builder()
                 .baseUrl("https://v3.velog.io")
@@ -47,8 +82,8 @@ public class VelogClient {
                     .comment(p.getComments_count())
                     .like(p.getLikes())
                     .img(p.getThumbnail())
-                    .tag(null) // Todo: 채워야 하는 데이터
-                    .url(null) // Todo: 채워야 하는 데이터
+                    .tag(null) // Comment: trend 게시물은 tag가 공백!
+                    .url("velog.io/@"+p.getUser().getUsername()+"/"+p.url_slug)
                     .build();
 
             trendPostList.add(postDto);
